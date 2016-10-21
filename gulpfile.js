a207d3a4897eccab7704c8f16281402629de6c7b
daniel <daniel@VipshopAlpha.local>
2016-10-21 15:11:47 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -98,15 +98,35 @@ gulp.task('serve', () => {
 
 gulp.task('zip', () => {
   var package = require('./package.json');
+  var env = process.env.NODE_ENV;                                                                                                                                                                
+  var server = { // XXX 请根据实际情况修改不同环境的静态服务器和提供JSONP接口的服务域名                                                                                                          
+    sit: {                                                                                                                                                                                       
+      static: 'http://viva.vipstatic.com',                                                                                                                                                       
+      jsonp: 'http://viva-api.vip.com'                                                                                                                                                           
+    },                                                                                                                                                                                           
+    uat: {                                                                                                                                                                                       
+      static: 'http://viva.vipstatic.com',                                                                                                                                                       
+      jsonp: 'http://viva-api.vip.com'                                                                                                                                                           
+    },                                                                                                                                                                                           
+    production: {                                                                                                                                                                                
+      static: 'http://viva.vipstatic.com',                                                                                                                                                       
+      jsonp: 'http://viva-api.vip.com'                                                                                                                                                           
+    }                                                                                                                                                                                            
+  }                                                                                                                                                                                              
+  var envServer = server[env] || server.sit; 
   return gulp.src(config.dist + '/**/*')
     .pipe(gulpif(file => {                                                                                                                                                                       
       var fileName = file.path.replace(config.dist + path.sep, '');                                                                                                                              
       return fileName === 'index.html';                                                                                                                                                          
-    }, replace(/=([a-z\.0-9]+\.(js|css))/g, '=http://viva.vipstatic.com/uploadfiles/viva-act-static/' + package.name + '/$1'))) // 替换JS/CSS路径。只处理index.html。 XXX 请根据实际修改替换的内容                              
+    }, replace(/=([a-z\.0-9]+\.(js|css))/g, '=' + envServer.static + '/uploadfiles/viva-act-static/' + package.name + '/$1'))) // 替换JS/CSS路径。只处理index.html XXX 请根据实际情况进行修改路径                              
     .pipe(gulpif(file => {                                                                                                                                                                       
       var fileName = file.path.replace(config.dist + path.sep, '');                                                                                                                              
       return fileName.match(/^style\.[a-z0-9]{8}\.css/) || fileName.match(/^app\.[a-z0-9]{8}\.js/);                                                                                              
-    }, replace(/img\/([a-zA-Z0-9\-_]+\.(jpg|png|gif|jpeg))/g, 'http://viva.vipstatic.com/uploadfiles/viva-act-static/' + package.name + '/img/$1'))) // 替换图片路径。只处理app.js和style.css。 XXX 请根据实际修改替换的内容     
+    }, replace(/img\/([a-zA-Z0-9\-_]+\.(jpg|png|gif|jpeg))/g, envServer.static + '/uploadfiles/viva-act-static/' + package.name + '/img/$1'))) // 替换图片路径。只处理app.js和style.css XXX 请根据实际情况进行修改路径        
+    .pipe(gulpif(file => {                                                                                                                                                                       
+      var fileName = file.path.replace(config.dist + path.sep, '');                                                                                                                              
+      return fileName.match(/^app\.[a-z0-9]{8}\.js/);                                                                                                                                            
+    }, replace(/http.jsonp\([a-z]{1}\+"(.*?)"\)/, 'http.jsonp("' + envServer.jsonp + '$1")'))) // 替换JSONP请求路径。只处理app.js 
     .pipe(gulp.dest(config.dist + '/' + package.name)) 
     .pipe(zip(package.name + '.zip'))
     .pipe(gulp.dest(config.dist));
--- src/common/rest-service.js
+++ src/common/rest-service.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+var jsonpServer = '';
+
 export function getTestName() {
   return Vue.http.get('/api/test/getName');
 };
@@ -7,6 +9,10 @@ export function getTestName() {
 export function getTestMsg() {
   return Vue.http.get('/api/test/getMsg');
 };
+
+export function getJSONPTest() {
+  return Vue.http.jsonp(jsonpServer + '/api/test/getJSONPTest');
+}
 
 
 
